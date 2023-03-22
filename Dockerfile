3c596205617634496e146e9714ebc3f4a2b7764f
Jochem Bruijns <jochem.bruijns@cyso.com>
2023-03-22 21:32:08 +0100

    Add netbox sync for kubernetes versions

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,8 @@
 FROM python:3.9
 
 # Variables
-ENV TINI_VERSION=v0.14.0 \
-    TINI_SHA256=eeffbe853a15982e85d923390d3a0c4c5bcf9c78635f4ddae207792fa4d7b5e6 \
-    CYSO_CA_SHA256=a8b86b5a4ee70cb08ee7c475fb4f43b922483adca41a591ba16d9cb4db78178c \
-    REQUESTS_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt" \
-    PIP_INDEX_URL="https://pypi.build.cyso.net/pip/live" \
-    PYTHONWARNINGS="ignore:Unverified HTTPS request"
-
-# Install Tini
-RUN curl -fSL -o /sbin/tini https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini \
- && echo "${TINI_SHA256} */sbin/tini" | sha256sum -c - \
- && chmod +x /sbin/tini
-ENTRYPOINT [ "/sbin/tini", "--" ]
+ENV CYSO_CA_SHA256=a8b86b5a4ee70cb08ee7c475fb4f43b922483adca41a591ba16d9cb4db78178c \
+    REQUESTS_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"
 
 # Install Cyso CA bundle
 RUN curl -fSL -o cyso-internal-ca.deb http://apt.cyso.net/cyso-internal-ca.deb \
@@ -24,6 +14,7 @@ RUN curl -fSL -o cyso-internal-ca.deb http://apt.cyso.net/cyso-internal-ca.deb \
 ADD requirements.txt /usr/src/app/
 RUN pip install --upgrade pip setuptools virtualenv tox \
  && pip install -r /usr/src/app/requirements.txt
+
 
 # Add application
 WORKDIR /usr/src/app
---
+++ prom2netbox/cli.py
@@ -0,0 +1,71 @@
+import os
+import logging
+import requests
+from pynetbox import api
+
+logger = logging.getLogger(__name__)
+prometheus_url = os.environ['PROMETHEUS_URL']
+netbox_url = os.environ['NETBOX_URL']
+netbox_token = os.environ['NETBOX_TOKEN']
+
+netbox = api(
+    netbox_url,
+    netbox_token)
+
+
+def configure_logging():
+    fmt = "%(asctime)s %(name)-24.24s -> [%(levelname)-7.7s] %(message)s"
+
+    logging.basicConfig(format=fmt)
+    root_logger = logging.getLogger(None)
+    root_logger.setLevel(logging.INFO)
+
+
+def main():
+    configure_logging()
+    kubernetes_versions = get_kubernetes_versions()
+    
+    seen = []
+    for i in kubernetes_versions:
+        if i['tenant'] in seen:
+            logger.error(f"Cluster {i['tenant']} ({i['version']}) duplicated in result. This cluster is running multiple versions")
+            continue
+        seen.append(i['tenant'])
+        
+        update_netbox_kubernetes_version(i['tenant'], i['version'])
+
+
+def get_kubernetes_versions():
+    response = requests.get(
+        f'{prometheus_url}/api/v1/query', 
+        params={'query': 'sum(kubernetes_build_info) by (tenant, minor, major)'}
+    )
+    response.raise_for_status()
+         
+    metrics = response.json()['data']['result']
+    
+    if len(metrics) < 1:
+        logger.warning("No results in prometheus response")
+        return 
+    
+    for i in metrics:
+        tenant = i['metric']['tenant']
+        version = f"v{i['metric']['major']}.{i['metric']['minor']}"
+        
+        yield {'tenant': tenant, 'version': version}
+
+
+def update_netbox_kubernetes_version(tenant, version):
+    cluster = netbox.virtualization.clusters.get(name=tenant)
+    if cluster is None:
+        logger.error(f"Cluster {tenant} not found in netbox")
+        return
+
+    cluster.custom_fields["k8s_version"] = version
+    cluster.save()
+
+
+
+
+if __name__ == "__main__":  # pragma: nocover
+    main()
